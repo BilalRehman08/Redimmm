32824f97c837c5ed9c1be277c5d3219fe2918b82
Bilal <bilal@Muhammads-MacBook-Pro.local>
2022-10-27 21:28:00 +0500

    project setup

---
+++ android/app/src/main/kotlin/com/example/redimm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.redimm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:redimm/views/home_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeView(),
+    );
+  }
+}
